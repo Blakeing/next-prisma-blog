c2614ec0ee4e8ece5e4b8882b9ff44234db04eb5
Blake Ingenthron <blakeingenthron@gmail.com>
2021-04-17 22:38:03 -0500

    ignored ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient
-}
 
 let prisma: PrismaClient
 
@@ -10,10 +7,12 @@ let prisma: PrismaClient
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient()
 } else {
+  // @ts-ignore
   if (!global.prisma) {
+    // @ts-ignore
     global.prisma = new PrismaClient()
   }
-
+  // @ts-ignore
   prisma = global.prisma
 }
 
